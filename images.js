fecccb19696a34fcc4cc936f3f43a050be51a4ce
Abhijit Rao <quasi@quasilabs.in>
2025-09-26 17:16:29 +0530

    Version 1.1 - changed styles and added more images

--- images.js
+++ images.js
@@ -1,31 +1,27 @@
 var imageList = [
     {
         img: "portrait_1.jpeg",
-        desc: "Minolta S414"
+        desc: "Mangya. Minolta S414"
     },
     {
         img: "portrait_10.jpeg",
-        desc: "Sony Nex-3, Minolta AF 50mm F 1.4"
+        desc: "Kaka. Sony Nex-3 + Minolta AF 50/1.4"
     },
     {
         img: "portrait_11.jpeg",
-        desc: ""
+        desc: "Ajju of the Naka"
     },
     {
         img: "portrait_12.jpeg",
         desc: ""
     },
     {
-        img: "portrait_13.jpeg",
-        desc: ""
-    },
-    {
         img: "portrait_14.jpeg",
-        desc: ""
+        desc: "Happy. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_15.jpeg",
-        desc: ""
+        desc: "Vasu. "
     },
     {
         img: "portrait_16.jpeg",
@@ -45,91 +41,87 @@ var imageList = [
     },
     {
         img: "portrait_2.jpeg",
-        desc: ""
-    },
-    {
-        img: "portrait_20.jpeg",
-        desc: ""
+        desc: "Vivek. Writer. Sony Nex 3 + Voigtlander Color Skopar 35/2.5"
     },
     {
         img: "portrait_21.jpeg",
-        desc: ""
+        desc: "Tariq"
     },
     {
         img: "portrait_22.jpeg",
-        desc: ""
+        desc: "Nishikant Kamat. Director. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_23.jpeg",
-        desc: ""
+        desc: "Appa. Sony A7R3 + Zeiss 50/1.4"
     },
     {
         img: "portrait_24.jpeg",
-        desc: ""
+        desc: "Sujay of the Naka. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_25.jpeg",
-        desc: ""
+        desc: "Percy. Sony Nex 3 + Minolta AF 50/1.4"
     },
     {
         img: "portrait_26.jpeg",
-        desc: ""
+        desc: "Sandeep Kulkarni. Actor. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_27.jpeg",
-        desc: ""
+        desc: "Dinesh. Sony Nex 3 + Minolta AF 50/1.4"
     },
     {
         img: "portrait_28.jpeg",
-        desc: ""
+        desc: "Happy. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_29.jpeg",
-        desc: ""
+        desc: "Himanshu. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_3.jpeg",
-        desc: ""
+        desc: "Mayur. Minolta S414"
     },
     {
         img: "portrait_30.jpeg",
-        desc: ""
+        desc: "Vijay of the Alter Egos. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_31.jpeg",
-        desc: ""
+        desc: "Harsh. Sony Nex 3 + Minolta AF 50/1.4"
     },
     {
         img: "portrait_32.jpeg",
-        desc: ""
+        desc: "Abha. Sony Nex 3 + Voigtlander Color Skopar 35/2.5"
     },
     {
         img: "portrait_33.jpeg",
-        desc: ""
+        desc: "Sujay."
     },
     {
         img: "portrait_34.jpeg",
-        desc: ""
+        desc: "Unmesh. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_35.jpeg",
-        desc: ""
+        desc: "Sanjay Jadhav. Cinematographer. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_36.jpeg",
-        desc: ""
+        desc: "Raju. Guitarist. "
     },
     {
         img: "portrait_37.jpeg",
-        desc: ""
+        desc: "Percy. Sony Nex 3 + Minolta AF 50/1.4"
     },
     {
         img: "portrait_38.jpeg",
-        desc: ""
+        desc: "Aniket. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_39.jpeg",
-        desc: ""
+        desc: "Soha. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_4.jpeg",
@@ -137,51 +129,43 @@ var imageList = [
     },
     {
         img: "portrait_40.jpeg",
-        desc: ""
+        desc: "Yo."
     },
     {
         img: "portrait_41.jpeg",
-        desc: ""
+        desc: "Nishikant Kamat. Director. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_42.jpeg",
-        desc: ""
+        desc: "Sandesh Jadhav. Actor. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_43.jpeg",
-        desc: ""
+        desc: "Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_44.jpeg",
-        desc: ""
+        desc: "Manohar. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_45.jpeg",
-        desc: ""
+        desc: "Nilesh. Sony Nex 3 + Voigtlander Color Skopar 35/2.5"
     },
     {
         img: "portrait_46.jpeg",
-        desc: ""
-    },
-    {
-        img: "portrait_47.jpeg",
-        desc: ""
-    },
-    {
-        img: "portrait_48.jpeg",
-        desc: ""
+        desc: "Shot and Dinu of the Naka. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_49.jpeg",
-        desc: ""
+        desc: "Aniket"
     },
     {
         img: "portrait_5.jpeg",
-        desc: ""
+        desc: "Adi. Sitarist."
     },
     {
         img: "portrait_50.jpeg",
-        desc: ""
+        desc: "Yo"
     },
     {
         img: "portrait_51.jpeg",
@@ -189,47 +173,27 @@ var imageList = [
     },
     {
         img: "portrait_52.jpeg",
-        desc: ""
-    },
-    {
-        img: "portrait_53.jpeg",
-        desc: ""
-    },
-    {
-        img: "portrait_54.jpeg",
-        desc: ""
+        desc: "Yo"
     },
     {
         img: "portrait_55.jpg",
-        desc: ""
-    },
-    {
-        img: "portrait_56.jpg",
-        desc: ""
+        desc: "Raju. Guitarist."
     },
     {
         img: "portrait_57.jpg",
-        desc: ""
+        desc: "Pradeep. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_58.jpg",
-        desc: ""
-    },
-    {
-        img: "portrait_59.jpg",
-        desc: ""
-    },
-    {
-        img: "portrait_6.jpeg",
-        desc: ""
+        desc: "Anna"
     },
     {
         img: "portrait_60.jpg",
-        desc: ""
+        desc: "Sandeep Kulkarni. Actor. Minolta Dynax 5 + Minolta 50/1.4"
     },
     {
         img: "portrait_7.jpeg",
-        desc: ""
+        desc: "Uncle"
     },
     {
         img: "portrait_8.jpeg",
@@ -237,6 +201,226 @@ var imageList = [
     },
     {
         img: "portrait_9.jpeg",
+        desc: "Yo"
+    },
+    {
+        img: "portrait_100.jpeg",
         desc: ""
-    }
+        },
+        {
+        img: "portrait_101.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_102.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_103.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_104.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_105.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_106.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_107.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_108.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_109.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_110.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_111.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_112.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_113.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_114.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_115.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_61.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_62.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_63.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_64.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_65.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_66.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_67.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_68.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_69.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_70.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_71.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_72.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_73.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_74.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_75.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_76.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_77.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_78.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_79.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_80.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_81.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_82.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_83.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_84.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_85.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_86.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_87.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_88.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_89.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_90.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_91.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_92.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_93.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_94.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_95.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_96.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_97.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_98.jpeg",
+        desc: ""
+        },
+        {
+        img: "portrait_99.jpeg",
+        desc: ""
+        }    
 ];
